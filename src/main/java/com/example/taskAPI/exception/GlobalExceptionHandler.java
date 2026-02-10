88b13806899ca7bfcac85c99373c1b0f1949ba68
David Svensson <davidsvson@gmail.com>
2026-02-10 13:06:59 +0100

    alternative for-loop

--- src/main/java/com/example/taskAPI/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/taskAPI/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.taskAPI.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,9 +16,18 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Map<String, String>> handleValidationsExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
 
-        ex.getBindingResult().getFieldErrors().forEach( error ->
-                errors.put(error.getField(), error.getDefaultMessage())
-        );
+        for (FieldError  error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+
+
+//        {
+//            "done" : "value has to be true of false"
+//
+//        }
+//        ex.getBindingResult().getFieldErrors().forEach( error ->
+//                errors.put(error.getField(), error.getDefaultMessage())
+//        );
 
         return ResponseEntity.badRequest().body(errors);
     }
